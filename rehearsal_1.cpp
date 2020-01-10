3437a8c4c5086376a7f72efd7eab9b8a5aee56ea
lab302-5 <lab302-5@labeng.local>
2020-01-10 14:46:47 +0700

    final

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    float GPA;
+    string name;
+    cout<<"What is your name? : ";
+    cin>>name;
+    cout<<"What is your GPA? : ";
+    cin>>GPA;
+    if(GPA>=3.5)
+    {
+        cout<<name<<" InW Jrim Jrim!!!";
+    }
+    else
+    {
+        cout<<"Try harder,"<<" "<<name<<"!!!";
+    }
+    return 0;
+}
